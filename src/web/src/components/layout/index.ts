2604adf03c5733e29fb6a6208ab187ac6645256a
Rajesh Ramachander <42115537+RajeshRamachander@users.noreply.github.com>
2025-03-06 20:32:37 +0530

    Add public-samples/create-agent-f16fhc/src/web/src/components/layout/index.ts

---
+++ src/web/src/components/layout/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Barrel file for layout components
+ * 
+ * This file exports all layout components to simplify imports throughout the application,
+ * implementing the "Component Organization" requirement specified in the technical documentation.
+ * These layout components provide the structural foundation for implementing the wireframe
+ * layouts defined in section 7.3 of the technical specifications.
+ */
+
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as Sidebar } from './Sidebar';
+export { default as PageHeader } from './PageHeader';
+export { default as AuthLayout } from './AuthLayout';
+export { default as MainLayout } from './MainLayout';
