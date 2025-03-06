2596a95c75669152d768486188458e050fdc5b31
Rajesh Ramachander <42115537+RajeshRamachander@users.noreply.github.com>
2025-03-06 20:30:19 +0530

    Add public-samples/create-agent-f16fhc/src/web/src/components/common/DatePicker/index.ts

---
+++ src/web/src/components/common/DatePicker/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Index file for DatePicker component
+ * 
+ * This file serves as a barrel export for the DatePicker component,
+ * making it easier to import from other parts of the application without
+ * having to reference the specific file.
+ * 
+ * Supports the Time Period Selection requirements as specified in
+ * Technical Specifications/2.2 FUNCTIONAL REQUIREMENTS TABLE/2.2.2
+ */
+
+import DatePicker from './DatePicker';
+
+export default DatePicker;
